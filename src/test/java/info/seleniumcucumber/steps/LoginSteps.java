249ab4c331044947e12753f035c5d5360f634a91
RavanSeyfulla <ravanseyfullayev@gmail.com>
2025-06-16 18:14:46 -0400

    Update LoginSteps.java

--- src/test/java/info/seleniumcucumber/steps/LoginSteps.java
+++ src/test/java/info/seleniumcucumber/steps/LoginSteps.java
@@ -5,11 +5,21 @@ import info.seleniumcucumber.pages.LoginPage;
 import io.cucumber.java.en.Given;
 import org.junit.Assert;
 
+// Step definitions class for Cucumber scenarios related to login functionality
+// Extends AbstractPage to inherit WebDriver and wait utilities
 public class LoginSteps extends AbstractPage {
 
+    // Cucumber step definition for: "Given I should get logged-in"
     @Given("^I should get logged-in$")
     public void should_logged_in() throws NoSuchFieldException {
+        // Create a new instance of LoginPage (Page Object)
         final LoginPage loginPage = new LoginPage();
-        Assert.assertEquals("You logged into a secure area!", loginPage.getMessage().getText().split("\n")[0].trim());
+
+        // Get the message element text and check if it contains expected login success message
+        // loginPage.getMessage() waits for the message element to load, then returns it
+        String actualMessage = loginPage.getMessage().getText().split("\n")[0].trim();
+
+        // Assertion to validate that the login was successful
+        Assert.assertEquals("You logged into a secure area!", actualMessage);
     }
 }
